a27bad8c25c2ffb3c941f4e96a6e63ad8a42540a
Uyouthe <melvoloskov@ya.ru>
2018-07-08 19:28:50 +0300

    init

---
+++ model.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql')
+
+var connection = mysql.createConnection({
+	host: process.env.JAWSDB_MARIA_URL || "127.0.0.1",
+	port: process.env.PORT || "3306",
+	user: process.env.USER,
+	password: process.env.PASSWORD
+})
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end()
