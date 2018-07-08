442b378df8d8905b6c123186fcec9bc361cb384b
Uyouthe <melvoloskov@ya.ru>
2018-07-08 22:43:41 +0300

    express sample added

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-require("./model.js")
+var express = require('express')
+var app = express()
+
+app.use(require('helmet')())
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(80, function () {
+  console.log('Example app listening on port 80!')
+})
