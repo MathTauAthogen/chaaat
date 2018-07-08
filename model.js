3f67794e2d27bfce89168f0ba137469e07b460f1
Uyouthe <melvoloskov@ya.ru>
2018-07-08 19:37:46 +0300

    trying to define production

--- model.js
+++ model.js
@@ -1,18 +1,18 @@
 var mysql = require('mysql')
+console.log(process.env.PRODUCTION)
+// var connection = mysql.createConnection({
+// 	host: process.env.JAWSDB_MARIA_URL || "127.0.0.1",
+// 	port: process.env.PORT || "3306",
+// 	user: process.env.USER,
+// 	password: process.env.PASSWORD
+// })
 
-var connection = mysql.createConnection({
-	host: process.env.JAWSDB_MARIA_URL || "127.0.0.1",
-	port: process.env.PORT || "3306",
-	user: process.env.USER,
-	password: process.env.PASSWORD
-})
+// connection.connect()
 
-connection.connect()
+// connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+//   if (err) throw err
 
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err
+//   console.log('The solution is: ', rows[0].solution)
+// })
 
-  console.log('The solution is: ', rows[0].solution)
-})
-
-connection.end()
+// connection.end()
